89e4ac52e97010568a8fd71d31fc34d6b69aa0ed
Dat Le-Tien <vnkeymaster@gmail.com>
2019-04-10 15:34:21 +0700

    Update API routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :guide
+
+  namespace :api do
+    namespace :v1 do
+      resources :guides do
+        collection do
+          get 'search'
+        end
+      end
+    end
+  end
 end
