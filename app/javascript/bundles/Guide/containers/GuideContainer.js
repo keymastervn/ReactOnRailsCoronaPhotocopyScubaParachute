4f364a6e67714384162e0eafc740633e4922482d
Dat Le-Tien <vnkeymaster@gmail.com>
2019-04-10 14:16:09 +0700

    Add react boilerplate for Guide

---
+++ app/javascript/bundles/Guide/containers/GuideContainer.js
@@ -0,0 +1,10 @@
+// Simple example of a React "smart" component
+
+import { connect } from 'react-redux';
+import Guide from '../components/Guide';
+import * as actions from '../actions/guideActionCreators';
+
+// Which part of the Redux global state does our component want to receive as props?
+const mapStateToProps = (state) => ({ name: state.name });
+
+export default connect(mapStateToProps, actions)(Guide);
