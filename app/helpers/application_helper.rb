6403550913b9b86ff85d89969366b506dddcf46c
Dat Le-Tien <vnkeymaster@gmail.com>
2019-04-21 23:49:10 +0700

    Add universal_cache_key

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def universal_cache_key
+    Rails.cache.fetch("It will be fu*king simple for using 2 cache layers", expires_in: 3.seconds ) do
+      [
+        Guide,
+        Language,
+        Activity,
+        GuidesActivity,
+        GuidesLanguage
+      ].map{|x| x.all.cache_key }.join("|")
+    end
+  end
 end
