ac3c24c2734d478516e73f5b1a10587f913062b5
Dat Le-Tien <vnkeymaster@gmail.com>
2019-04-22 00:40:48 +0700

    Add form to make Review

--- app/javascript/bundles/Guide/components/ShowGuide.jsx
+++ app/javascript/bundles/Guide/components/ShowGuide.jsx
@@ -4,6 +4,20 @@ import React from 'react';
 class ShowGuide extends React.Component {
   constructor(props, context) {
     super(props, context)
+
+    this.state = {value: ''};
+
+    this.handleReviewChange = this.handleReviewChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleReviewChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
   }
 
   render() {
@@ -12,7 +26,7 @@ class ShowGuide extends React.Component {
       <div className="container pt-4">
         <a href={"/guide"}>Click here to go to home page</a>
 
-        <div class="jumbotron">
+        <div className="jumbotron">
           <h1>Guide Homepage</h1>
           <hr />
           <p>Please tell guide to be honest and polite to all travelers</p>
@@ -20,7 +34,20 @@ class ShowGuide extends React.Component {
           <p> This is <b>{guide.email}</b></p>
           <p> He knows: <b>{guide.display_languages}</b> </p>
           <p> He can: <b>{guide.display_activities}</b> </p>
+          <p> His rating: </p>
         </div>
+
+        <h3> He would like to get your appreciation </h3>
+        <form onSubmit={this.handleFormSubmit}>
+          <div className="form-group">
+            <label htmlFor="reviewInput">
+              Comment
+              <input id="reviewInput" type="text" className="form-control" value={this.state.comment} onChange={this.handleReviewChange} />
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+
       </div>
     )
   }
