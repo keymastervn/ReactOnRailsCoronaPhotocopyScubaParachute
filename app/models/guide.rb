270e1ae906fd7b0255d1fd8b6b5f44e7f31cbe07
Dat Le-Tien <vnkeymaster@gmail.com>
2019-04-21 14:49:41 +0700

    Add methods for getting activities and languages in Guide model

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -4,4 +4,12 @@ class Guide < ApplicationRecord
   has_many :reviews
 
   validates_uniqueness_of :email
+
+  def display_languages
+    languages.collect(&:code).join(", ")
+  end
+
+  def display_activities
+    activities.collect(&:name).join(", ")
+  end
 end
