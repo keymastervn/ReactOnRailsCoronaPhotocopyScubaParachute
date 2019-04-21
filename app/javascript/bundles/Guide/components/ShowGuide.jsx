03239458563d7405ff5a9339a7e314c8ded79c39
Dat Le-Tien <vnkeymaster@gmail.com>
2019-04-22 00:48:53 +0700

    Change input to textArea and put the StarRating

--- app/javascript/bundles/Guide/components/ShowGuide.jsx
+++ app/javascript/bundles/Guide/components/ShowGuide.jsx
@@ -1,18 +1,29 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import StarRatings from 'react-star-ratings';
 
 class ShowGuide extends React.Component {
   constructor(props, context) {
     super(props, context)
 
-    this.state = {value: ''};
+    this.state = {
+      value: '',
+      rating: 0
+    };
 
     this.handleReviewChange = this.handleReviewChange.bind(this);
+    this.changeRating = this.changeRating.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleReviewChange(event) {
     this.setState({value: event.target.value});
+  }
+
+  changeRating( newRating, name ) {
+    this.setState({
+      rating: newRating
+    });
   }
 
   handleSubmit(event) {
@@ -42,9 +53,21 @@ class ShowGuide extends React.Component {
           <div className="form-group">
             <label htmlFor="reviewInput">
               Comment
-              <input id="reviewInput" type="text" className="form-control" value={this.state.comment} onChange={this.handleReviewChange} />
             </label>
+            <textarea id="reviewInput" type="text" className="form-control" value={this.state.comment} onChange={this.handleReviewChange} rows="4" />
           </div>
+
+          <div className="form-group">
+            <StarRatings
+              rating={this.state.rating}
+              starRatedColor="blue"
+              changeRating={this.changeRating}
+              numberOfStars={6}
+              starDimension="25px"
+              name='rating'
+            />
+          </div>
+
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
 
