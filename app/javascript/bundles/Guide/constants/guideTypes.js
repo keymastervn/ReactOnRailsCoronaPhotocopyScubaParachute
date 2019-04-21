4f8e973fb746ea3aa429d35d02372ccee39fe84d
Dat Le-Tien <vnkeymaster@gmail.com>
2019-04-21 19:12:06 +0700

    Add constants for types and apis

---
+++ app/javascript/bundles/Guide/constants/guideTypes.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+
+export const FETCH_DATA_ON_SEARCH_COMPLETED = 'FETCH_DATA_ON_SEARCH_COMPLETED';
+export const FETCH_DATA_ON_SEARCH_TO_STORE = 'FETCH_DATA_ON_SEARCH_TO_STORE';
