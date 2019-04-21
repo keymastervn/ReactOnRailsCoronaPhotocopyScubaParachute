d9581eb18a4468fffd60228e91df5826cb8d8ee2
Dat Le-Tien <vnkeymaster@gmail.com>
2019-04-22 00:02:59 +0700

    Add GuidesController#show

--- app/controllers/guide_controller.rb
+++ app/controllers/guide_controller.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
 class GuideController < ApplicationController
+  before_action :set_user, only: :show
+
   layout "guide_management"
 
   def index
     @props = { guides: GuideService.new.all }
   end
+
+  def show
+    @props = { guide: @guide }
+  end
+
+  private
+
+  def set_guide
+    @guide = Guide.find(params[:id])
+  end
+
 end
