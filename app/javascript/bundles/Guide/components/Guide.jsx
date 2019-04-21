6b80afc3d03f15cb6d7a5b4b862df34014f1661b
Dat Le-Tien <vnkeymaster@gmail.com>
2019-04-21 14:31:56 +0700

    Add customPagination

--- app/javascript/bundles/Guide/components/Guide.jsx
+++ app/javascript/bundles/Guide/components/Guide.jsx
@@ -1,10 +1,34 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
-import paginationFactory from 'react-bootstrap-table2-paginator';
+import paginationFactory, { PaginationProvider, PaginationTotalStandalone, PaginationListStandalone, SizePerPageDropdownStandalone } from 'react-bootstrap-table2-paginator';
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
 
 const { SearchBar } = Search;
+
+const sizePerPageRenderer = ({
+  options,
+  currSizePerPage,
+  onSizePerPageChange
+}) => (
+  <div className="btn-group" role="group">
+    {
+      options.map((option) => {
+        const isSelect = currSizePerPage === `${option.page}`;
+        return (
+          <button
+            key={ option.text }
+            type="button"
+            onClick={ () => onSizePerPageChange(option.page) }
+            className={ `btn ${isSelect ? 'btn-primary' : 'btn-warning'}` }
+          >
+            { option.text }
+          </button>
+        );
+      })
+    }
+  </div>
+);
 
 const columns = [{
   dataField: 'id',
@@ -98,6 +122,18 @@ class HelloWorld extends React.Component {
   render() {
     const { guides } = this.props;
     const { updateName } = this.props;
+    const options = {
+      custom: true,
+      totalSize: this.state.guides.length,
+      hideSizePerPage: false,
+      sizePerPageList: [{
+        text: '5', value: 5
+      }, {
+        text: '10', value: 10
+      }, {
+        text: 'All', value: this.state.guides.length
+      }]
+    };
 
     const invisibleTestComp = (
       <div>
@@ -119,7 +155,7 @@ class HelloWorld extends React.Component {
       </div>
     )
     return (
-      <div className="container">
+      <div className="container pt-4">
         <ToolkitProvider
           keyField="id"
           data={ this.state.guides }
@@ -130,17 +166,50 @@ class HelloWorld extends React.Component {
           {
             props => (
               <div>
-                <SearchBar
-                  { ...props.searchProps }
-                  delay={ 1000 }
-                />
-                <hr />
-                <BootstrapTable { ...props.baseProps }
-                  pagination={ paginationFactory() }
-                  striped
-                  hover
-                  condensed
-                />
+                <PaginationProvider
+                  pagination={ paginationFactory(options) }
+                >
+                  {
+                    ({
+                      paginationProps,
+                      paginationTableProps
+                    }) => (
+                      <div>
+                        <span style={{display: 'inline', float: 'left'}}>
+                          <SizePerPageDropdownStandalone
+                            { ...paginationProps }
+                          />
+                        </span>
+                        <span style={{display: 'inline', float: 'right'}}>
+                          <SearchBar
+                            { ...props.searchProps }
+                            delay={ 1000 }
+                          />
+                        </span>
+
+                        <div>
+                          <BootstrapTable { ...props.baseProps }
+                          { ...paginationTableProps }
+                            striped
+                            hover
+                            condensed
+                          />
+                        </div>
+
+                        <span style={{display: 'inline', float: 'left'}}>
+                          <PaginationTotalStandalone
+                            { ...paginationProps }
+                          />
+                        </span>
+                        <span style={{display: 'inline', float: 'right'}}>
+                          <PaginationListStandalone
+                            { ...paginationProps }
+                          />
+                        </span>
+                      </div>
+                    )
+                  }
+                </PaginationProvider>
               </div>
             )
           }
