05f8a94eee7fdcea6797f46b7744d3d76513a658
Dat Le-Tien <vnkeymaster@gmail.com>
2019-04-21 10:39:46 +0700

    Add css to ReactBootStrapTable component

--- app/javascript/packs/main-bundle.js
+++ app/javascript/packs/main-bundle.js
@@ -3,6 +3,10 @@ import ReactOnRails from 'react-on-rails';
 import HelloWorldApp from '../bundles/HelloWorld/startup/HelloWorldApp';
 import GuideApp from '../bundles/Guide/startup/GuideApp';
 
+// Support bootstrap
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+
+
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   HelloWorldApp,
