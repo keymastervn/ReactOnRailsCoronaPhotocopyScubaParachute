c5591aff869caee6b0916baf1a1ee0598819d3e1
Dat Le-Tien <vnkeymaster@gmail.com>
2019-04-21 17:33:45 +0700

    Refactor ReactBootstrapTable into RemoteComp

--- app/javascript/bundles/Guide/components/Guide.jsx
+++ app/javascript/bundles/Guide/components/Guide.jsx
@@ -68,84 +68,103 @@ class Guide extends React.Component {
           text: 'All', value: this.props.guides.length
         }]
       },
-      data: this.props.guides
+      data: this.props.guides.slice(0, 5),
+      page: 1,
+      totalSize: this.props.guides.length,
+      sizePerPage: 5
+    }
+
+    this.handleTableChange = this.handleTableChange.bind(this)
+    this.fetchData = this.fetchData.bind(this)
+  }
+
+  fetchData() {
+    if (this.node.props.search.searchText !== undefined && this.node.props.search.searchText !== "") {
+      // TODO Search remote
     }
   }
 
+  handleTableChange(type, { page, sizePerPage, filters, sortField, sortOrder, cellEdit }) {
+    var self = this;
+    const currentIndex = (page - 1) * sizePerPage;
+
+    this.fetchData()
+
+    setTimeout(() => {
+      // Handle cell editing
+      if (type === 'cellEdit') {
+        const { rowId, dataField, newValue } = cellEdit;
+        this.props.guides = this.props.guides.map((row) => {
+          if (row.id === rowId) {
+            const newRow = { ...row };
+            newRow[dataField] = newValue;
+            return newRow;
+          }
+          return row;
+        });
+      }
+      let result = this.props.guides;
+
+      // Handle column filters
+      result = result.filter((row) => {
+        let valid = true;
+
+        for (const dataField in filters) {
+          const { filterVal, filterType, comparator } = filters[dataField];
+
+          if (filterType === 'TEXT') {
+            if (comparator === Comparator.LIKE) {
+              valid = row[dataField].toString().indexOf(filterVal) > -1;
+            } else {
+              valid = row[dataField] === filterVal;
+            }
+          }
+          if (!valid) break;
+        }
+        return valid;
+      });
+      // Handle column sort
+      if (sortOrder === 'asc') {
+        result = result.sort((a, b) => {
+          if (a[sortField] > b[sortField]) {
+            return 1;
+          } else if (b[sortField] > a[sortField]) {
+            return -1;
+          }
+          return 0;
+        });
+      } else {
+        result = result.sort((a, b) => {
+          if (a[sortField] > b[sortField]) {
+            return -1;
+          } else if (b[sortField] > a[sortField]) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+      this.setState(() => ({
+        page,
+        data: result.slice(currentIndex, currentIndex + sizePerPage),
+        totalSize: result.length,
+        sizePerPage
+      }));
+    }, 300);
+  }
+
   render() {
-    const data = this.state.data;
-
-    const CaptionElement = () => <h3 style={{
-        borderRadius: '0.25em',
-        textAlign: 'center',
-        color: 'purple',
-        border: '1px solid purple',
-        padding: '0.5em' }}>
-        Tourguide Management
-      </h3>;
-
     return (
       <div className="container pt-4">
-        <ToolkitProvider
-          keyField="id"
-          data={ data }
+        <RemoteAll
           columns={ this.state.columns }
-          search
-          bootstrap4
-        >
-          {
-            props => (
-              <div>
-                <PaginationProvider
-                  pagination={ paginationFactory(this.state.options) }
-                >
-                  {
-                    ({
-                      paginationProps,
-                      paginationTableProps
-                    }) => (
-                      <div>
-                        <span style={{display: 'inline', float: 'left'}}>
-                          <SizePerPageDropdownStandalone
-                            { ...paginationProps }
-                          />
-                        </span>
-                        <span style={{display: 'inline', float: 'right'}}>
-                          <SearchBar
-                            { ...props.searchProps }
-                            delay={ 1000 }
-                          />
-                        </span>
-
-                        <div>
-                          <BootstrapTable { ...props.baseProps }
-                            { ...paginationTableProps }
-                            caption={<CaptionElement />}
-                            striped
-                            hover
-                            condensed
-                          />
-                        </div>
-
-                        <span style={{display: 'inline', float: 'left'}}>
-                          <PaginationTotalStandalone
-                            { ...paginationProps }
-                          />
-                        </span>
-                        <span style={{display: 'inline', float: 'right'}}>
-                          <PaginationListStandalone
-                            { ...paginationProps }
-                          />
-                        </span>
-                      </div>
-                    )
-                  }
-                </PaginationProvider>
-              </div>
-            )
-          }
-        </ToolkitProvider>
-
+          options={ this.state.options }
+          data={ this.state.data }
+          page={ this.state.page }
+          sizePerPage={ this.state.sizePerPage }
+          totalSize={ this.state.totalSize }
+          onTableChange={ this.handleTableChange }
+          container={this}
+        />
       </div>
     )
   }
@@ -155,5 +174,81 @@ Guide.propTypes = {
   guides: PropTypes.array.isRequired,
   updateName: PropTypes.func.isRequired,
 };
+
+const CaptionElement = () => <h3 style={{
+  borderRadius: '0.25em',
+  textAlign: 'center',
+  color: 'purple',
+  border: '1px solid purple',
+  padding: '0.5em' }}>
+  Tourguide Management
+</h3>;
+
+const RemoteAll = ({ data, page, sizePerPage, onTableChange, totalSize, columns, options, container }) => (
+  <ToolkitProvider
+    keyField="id"
+    data={ data }
+    columns={ columns }
+    search
+    bootstrap4
+  >
+    {
+      props => (
+        <div>
+          <PaginationProvider
+            pagination={ paginationFactory(
+              Object.assign({}, options)
+            ) }
+          >
+            {
+              ({
+                paginationProps,
+                paginationTableProps
+              }) => (
+                <div>
+                  <span style={{display: 'inline', float: 'left'}}>
+                    <SizePerPageDropdownStandalone
+                      { ...paginationProps }
+                    />
+                  </span>
+                  <span style={{display: 'inline', float: 'right'}}>
+                    <SearchBar
+                      { ...props.searchProps }
+                      // delay={ 1000 }
+                    />
+                  </span>
+
+                  <div>
+                    <BootstrapTable { ...props.baseProps }
+                      { ...paginationTableProps }
+                      ref={ n => container.node = n }
+                      caption={<CaptionElement />}
+                      striped
+                      hover
+                      condensed
+                      remote
+                      onTableChange={ onTableChange }
+                    />
+                  </div>
+
+                  <span style={{display: 'inline', float: 'left'}}>
+                    <PaginationTotalStandalone
+                      { ...paginationProps }
+                    />
+                  </span>
+                  <span style={{display: 'inline', float: 'right'}}>
+                    <PaginationListStandalone
+                      { ...paginationProps }
+                    />
+                  </span>
+                </div>
+              )
+            }
+          </PaginationProvider>
+        </div>
+      )
+    }
+  </ToolkitProvider>
+)
 
 export default Guide;
